dcf688e4816eaf0302a47d9aae16b0da31d08dec
Csibe17 <csehgabor17@gmail.com>
2013-04-26 17:44:16 +0200

    GameActivity layout finished
    TODO: X, O and blank field graphics

--- TicTacToe/src/hu/medtech/tictactoe/GameActivity.java
+++ TicTacToe/src/hu/medtech/tictactoe/GameActivity.java
@@ -1,8 +1,12 @@
 package hu.medtech.tictactoe;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class GameActivity extends Activity {
 
@@ -10,13 +14,29 @@ public class GameActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.game, menu);
-		return true;
+		final ImageAdapter imad = new ImageAdapter(this);
+
+		final GridView gridview = (GridView) findViewById(R.id.gamegridview);
+		gridview.setAdapter(imad);
+
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+
+				Toast.makeText(GameActivity.this,
+						"pos:" + position + " count:" + imad.getCount(),
+						Toast.LENGTH_SHORT).show();
+
+				// create message
+				MessageContainer m = new MessageContainer();
+				m.setMessage(position % 2 + 2);
+				// refresh gridview elements
+				imad.addElement(m, position);
+				imad.notifyDataSetChanged();
+			}
+		});
+
 	}
 
 }
---
+++ TicTacToe/src/hu/medtech/tictactoe/ImageAdapter.java
@@ -0,0 +1,71 @@
+package hu.medtech.tictactoe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+
+	private Context mContext;
+	private List<Integer> mThumbIds = new ArrayList<Integer>();
+
+	public ImageAdapter(Context c) {
+		mContext = c;
+		for (int i = 0; i < 140; i++) {
+			mThumbIds.add(R.drawable.field_blank);
+		}
+	}
+
+	public void addElement(MessageContainer m, int pos) {
+		switch (m.getMessage()) {
+		case MessageContainer.MESSAGE_SYMBOL_X:
+			mThumbIds.set(pos, R.drawable.field_x);
+			break;
+		case MessageContainer.MESSAGE_SYMBOL_Y:
+			mThumbIds.set(pos, R.drawable.field_y);
+			break;
+		}
+
+	}
+
+	public int getCount() {
+		return mThumbIds.size();
+	}
+
+	public Object getItem(int position) {
+		return null;
+	}
+
+	public long getItemId(int position) {
+		return 0;
+	}
+
+	// create a new ImageView for each item referenced by the Adapter
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ImageView imageView;
+		if (convertView == null) { // if it's not recycled, initialize some
+									// attributes
+			imageView = new ImageView(mContext);
+			GridView.LayoutParams params = new GridView.LayoutParams(
+					GridView.LayoutParams.FILL_PARENT,
+					GridView.LayoutParams.WRAP_CONTENT);
+			imageView.setLayoutParams(new GridView.LayoutParams(params));
+			imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+			imageView.setAdjustViewBounds(true);
+			imageView.setPadding(0, 0, 0, 0);
+
+		} else {
+			imageView = (ImageView) convertView;
+		}
+
+		imageView.setImageResource(mThumbIds.get(position));
+		return imageView;
+	}
+
+}
