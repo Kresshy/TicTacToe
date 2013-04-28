4385db13064a6f2d01c3bda96445e297f0e528d9
Csibe17 <csehgabor17@gmail.com>
2013-04-28 18:09:18 +0200

    Don't needed menus

--- TicTacToe/src/hu/medtech/tictactoe/OptionsActivity.java
+++ TicTacToe/src/hu/medtech/tictactoe/OptionsActivity.java
@@ -12,11 +12,4 @@ public class OptionsActivity extends Activity {
 		setContentView(R.layout.activity_options);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.options, menu);
-		return true;
-	}
-
 }
